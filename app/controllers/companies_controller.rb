57ea0531966133e0bcf1e765cde45f20dba5d295
Tsutomu Saito <centurism@gmail.com>
2016-05-02 02:43:09 +0900

    ちょっとだけいじりましたが、前回と大きな変更はないです。

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,9 +7,13 @@ class CompaniesController < ApplicationController
 
   def search
     @companies = Company.all
+    @companies = Company.search(params[:q])
     @companies = Company.search(rate_lt: params[:q][:rate_lt], review_lt: params[:q][:review_lt]).result
   end
 
-
+  # private
+  # def search_params
+  #   params.require(:company).permit(rate_lt: params[:q][:rate_lt], review_lt: params[:q][:review_lt])
+  # end
 
 end
